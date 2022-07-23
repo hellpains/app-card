4175f9d1868303a29862f1655a486b732dca827b
ShuVeriDA <bashtarov@outlook.com>
2022-07-23 12:43:55 +0300

    test

--- src/features/Registration/Registration.tsx
+++ src/features/Registration/Registration.tsx
@@ -1,9 +1,82 @@
 import React from 'react';
+import classes from './Registration.module.css'
+import {NavLink} from "react-router-dom";
+import {useFormik} from "formik";
+
+type FormikErrorType = {
+   email?: string
+   password?: string
+}
 
 export const Registration = () => {
-    return (
-        <div>
-            Registration
-        </div>
-    );
+
+   const formik = useFormik({
+      initialValues: {
+         email: '',
+         password: '',
+         rememberMe: false
+      },
+      validate: (values) => {
+         const errors: FormikErrorType = {};
+         if (!values.email) {
+            errors.email = 'Required';
+         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+         }
+
+         if (!values.password) {
+            errors.password = 'Required'
+         } else if (values.password.length < 8) {
+            errors.password = 'Password must be more than 8 characters'
+         }
+         return errors;
+      },
+      onSubmit: values => {
+         // dispatch(loginTC(values))
+         formik.resetForm()
+      },
+   })
+
+   return (
+      <div className={classes.regWrapper}>
+         <div className={classes.reg}>
+            <form onSubmit={formik.handleSubmit}>
+               <h1 className={classes.title}>Sign Up</h1>
+               <div>
+                  <input className={classes.inputEmail}
+                         type="email"
+                         placeholder={'Email'}
+                         {...formik.getFieldProps('email')}
+                  />
+                  {formik.touched.email && formik.errors.email && <span style={{color: "red"}}>{formik.errors.email}</span>}
+               </div>
+
+               <div>
+                  <input className={classes.inputPass}
+                         type="password"
+                         placeholder={'Password'}
+                         {...formik.getFieldProps('password')}
+                  />
+               </div>
+               {formik.touched.password && formik.errors.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
+               <div>
+                  <input className={classes.inputPassConfirm}
+                         type="passwordConfirm"
+                         placeholder={'Confirm password'}
+                         {...formik.getFieldProps('passwordConfirm')}
+                  />
+                  {formik.touched.password && formik.errors.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
+               </div>
+               <NavLink to={'/profile'} className={classes.btn}>Sign Up</NavLink>
+            </form>
+            <div className={classes.signIn}>
+               <span className={classes.info}>Don't have an account?</span>
+               <NavLink to={'/login'} className={classes.signInLink}>
+                  Sign In
+               </NavLink>
+            </div>
+         </div>
+
+      </div>
+   );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,18 @@
 import {AnyAction, combineReducers, createStore} from "redux";
 import {ThunkDispatch} from "redux-thunk";
-import {useDispatch} from "react-redux";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import {registrationReducer} from "./reducers/registrationReducer";
 
 export type DispatchType  = ThunkDispatch<AppRootStateType, unknown, AnyAction>
 export const useAppDispatch = () => useDispatch<DispatchType>()
 
 export const rootReducer = combineReducers({
-
+ registration: registrationReducer,
 })
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 export const store = createStore(rootReducer)
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 
 
 export type AppActionsType={}
