8cc36b893b0169ed38cf77f588bf60329579e264
hellpains <rustam2004sadulaev@mail.ru>
2022-07-23 13:01:24 +0300

    prepared api

--- src/api/cards-api.ts
+++ src/api/cards-api.ts
@@ -1,33 +1,52 @@
 import axios from "axios"
 
-export const todolistsAPI = {
-    // getTodolists() {
-    //     return instance.get<TodolistType[]>(`todo-lists`) /////пример
-    // }
-}
+export const cardsAPI = {}
 
 export const authAPI = {
-    login() {
-        return instance.post('auth/login')
+    // отправить на сервер email and password
+    register(data: any) {
+        return instance.post('auth/register', data)
     },
+
+    // отправить на сервер email, password and rememberMe
+    login(data: any) {
+        return instance.post('auth/login', data)
+    },
+
+    // отправить на сервер пустой объект
     logout() {
         return instance.delete<ResponseType>('auth/login')
     },
-    me(){
-        return instance.get('auth/me')
-    }
+
+    // отправить на сервер пустой объект
+    me() {
+        return instance.post('auth/me')
+    },
+
+    // отправить на сервер name or avatar (url or base64)
+    updatedProfile() {
+        return instance.put('/auth/me')
+    },
+
+    // отправить на сервер email, from, message
+    forgotPassword() {
+        return instance.post('auth/me')
+    },
 }
 
+
 // types
-
-
+// type UpdatedProfileType = {
+//     updatedUser: {
+//         name:string,
+//         avatar:string
+//     }
+//     error?: string
+// }
 
 
 // settings
-const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+export const instance = axios.create({
+    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
     withCredentials: true,
-    headers: {
-        'API-KEY': '37224276-0334-42ce-b08a-d82d9d4f8d89'
-    }
 })
